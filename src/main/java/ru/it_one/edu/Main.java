ae42463e3a505e697fc90e86260d30fe5f5f257c
Овсянников Александр Александрович <147491618+Gan9leri@users.noreply.github.com>
2024-04-16 15:59:08 +0300

    Update Main.java
    Меняю 5 строчку

--- src/main/java/ru/it_one/edu/Main.java
+++ src/main/java/ru/it_one/edu/Main.java
@@ -1,7 +1,7 @@
 package ru.it_one.edu;
 
 public class Main {
-    private static String msg = ""Hello qa_guru!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"";
+    private static String msg = ""Hello qa_guru!!!!!!!error!!!!!!!!!!!!!!!!!"";
     public static void main(String[] args) {
         System.out.println(msg);
     }
